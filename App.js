30b2ad390866002ecea86217fa7fcc9d78ec973c
AMOS <108424978+Ramaunspoken@users.noreply.github.com>
2022-11-22 17:50:17 +0300

    Update App.js

--- App.js
+++ App.js
@@ -1,4 +1,13 @@
 /* eslint-disable linebreak-style */
+// eslint-disable-next-line no-unused-vars
+function dcl(t = 'div') {
+  if (!t) {
+    t = 'div';
+  }
+  return document.createElement(t);
+}
+
+/* eslint-disable linebreak-style */
 const mobileMenu = document.querySelector('.header_menu');
 const navItems = document.querySelector('.mobile_nav_items');
 const selectNavItems = document.querySelectorAll('#m_nav');
@@ -21,3 +30,232 @@ selectNavItems.forEach((item) => {
   // eslint-disable-next-line linebreak-style
   item.addEventListener('click', toggleNav);
 });
+// eslint-disable-next-line no-unused-vars
+const projectData = [
+  {
+    id: 'project1',
+    title: 'Tonic',
+    frame: ['CANOPY', 'Back End Dev', 2015],
+    primaryText: 'A daily selection of privately personalized reads; no accounts\n or sign-ups required.',
+    tags: ['HTML', 'CSS', 'Javascript'],
+    imageUrl: './images/Snapshoot Portfolio.svg',
+    projectDetails: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
+    liveLink: '#',
+    sourceLink: '#',
+  },
+  {
+    id: 'project2',
+    title: 'Multi-Post Stories',
+    frame: ['FACEBOOK', 'FULL STACK DEV', 2015],
+    primaryText: 'A daily selection of privately personalized reads; no accounts\n or sign-ups required.',
+    tags: ['HTML', 'CSS', 'Javascript'],
+    imageUrl: './images/Snapshoot Portfolio (1).svg',
+    projectDetails: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
+    liveLink: '#',
+    sourceLink: '#',
+  },
+  {
+    id: 'project3',
+    title: 'Tonic',
+    frame: ['CANOPY', 'Back End Dev', 2015],
+    primaryText: 'A daily selection of privately personalized reads; no accounts\n or sign-ups required.',
+    tags: ['HTML', 'CSS', 'Javascript'],
+    imageUrl: './images/Snapshoot Portfolio (2).svg',
+    projectDetails: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
+    liveLink: '#',
+    sourceLink: '#',
+  },
+  {
+    id: 'project4',
+    title: 'Multi-Post Stories',
+    frame: ['FACEBOOK', 'FULL STACK DEV', 2015],
+    primaryText: 'A daily selection of privately personalized reads; no accounts\n or sign-ups required.',
+    tags: ['HTML', 'CSS', 'Javascript'],
+    imageUrl: './images/Snapshoot Portfolio (3).svg',
+    projectDetails: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
+    liveLink: '#',
+    sourceLink: '#',
+  },
+]; // End of portfolio data
+
+// eslint-disable-next-line no-unused-vars
+function fetchOnePoject(id) {
+  const projects = projectData;
+
+  let project;
+  for (let i = 0; i < projectData.length; i += 1) {
+    if (projects[i].id === id) {
+      project = projects[i];
+    }
+  }
+
+  if (project) {
+    const article = dcl('article');
+    article.classList.add('popup_article');
+    article.setAttribute('id', project.id);
+
+    const articleModal = dcl();
+    articleModal.classList.add('article-modal');
+    // Article title
+    const workTitle = dcl('h2');
+    workTitle.classList.add('work_title');
+    workTitle.innerText = project.title;
+
+    // cross-icon
+    const crossIcon = dcl('span');
+    crossIcon.setAttribute('id', 'article-close');
+    crossIcon.innerHTML = '<img src="./images/Icon - Cancel.png" alt="X"/>';
+
+    // work_info
+    const workInfo = dcl('ul');
+    workInfo.classList.add('work_info');
+
+    // work_info_item
+    project.frame.forEach((f) => {
+      const workInfoItem = dcl('li');
+      workInfoItem.classList.add('w_info_item');
+      workInfoItem.innerText = f;
+      workInfo.appendChild(workInfoItem);
+    });
+
+    // image
+    const articleImage = dcl();
+    articleImage.classList.add('article-image');
+    articleImage.innerHTML = `<img class='article-img' src='${project.imageUrl}' alt='${project.title}'/>`;
+
+    // project block
+    const projectBlock = dcl();
+    projectBlock.classList.add('article-block');
+
+    // left block
+    const leftBlock = dcl();
+    leftBlock.classList.add('left-block');
+    // article_details_content
+    const workDetailsContent = dcl('p');
+    workDetailsContent.classList.add('work_details_content');
+    workDetailsContent.innerText = project.projectDetails;
+    leftBlock.append(workDetailsContent);
+
+    // right block
+    const rightBlock = dcl();
+    rightBlock.classList.add('right-block');
+
+    // work_cat
+    const workCat = dcl('ul');
+    workCat.classList.add('work_cats');
+    project.tags.forEach((tag) => {
+      const catLi = dcl('li');
+      catLi.innerText = tag;
+      workCat.appendChild(catLi);
+    });
+
+    // actions
+    const actions = dcl();
+    actions.classList.add('actions');
+
+    // live link
+    const liveLink = dcl('a');
+    liveLink.classList.add('article-btn');
+    liveLink.setAttribute('href', project.liveLink);
+    liveLink.innerHTML = 'See Live <span class="btn-icon"><img src="./images/btn-live.png" alt= "Live"/></span>';
+
+    // source link
+    const sourceLink = dcl('a');
+    sourceLink.classList.add('article-btn');
+    sourceLink.setAttribute('href', project.sourceLink);
+    sourceLink.innerHTML = 'See Live <span class="btn-icon"><img src="./images/btn-github1.png" alt= "Live"/></span>';
+
+    // appending link
+    actions.append(liveLink, sourceLink);
+
+    // appending rightBlock
+    rightBlock.append(workCat, actions);
+
+    // appending projectBlock
+    projectBlock.append(leftBlock, rightBlock);
+
+    // appending article-modal
+    articleModal.append(crossIcon, workTitle, workInfo, articleImage, projectBlock);
+
+    // appending article-modal to article
+    article.append(articleModal);
+    // end of if condition
+    document.querySelector('main').append(article);
+
+    // add event
+    const closeModal = document.getElementById('article-close');
+    closeModal.addEventListener('click', () => {
+      document.querySelector('main').removeChild(article);
+    });
+  }
+}
+
+function fetchAllProject() {
+  // select the portfolio
+  const portfolio = document.getElementById('portfolio');
+  projectData.forEach((project) => {
+    // Dom element for card
+    const card = dcl();
+    card.classList.add('work_card');
+
+    const workPreview = dcl();
+    workPreview.classList.add('work_preview');
+    workPreview.innerHTML = `<img class='project-img' src='${project.imageUrl}' alt='${project.title}'/>`;
+    card.appendChild(workPreview);
+
+    // work_details
+    const workDetails = dcl();
+    workDetails.classList.add('work_details');
+
+    // work title
+    const workTitle = dcl('h2');
+    workTitle.classList.add('work_title');
+    workTitle.innerText = project.title;
+    workDetails.appendChild(workTitle);
+
+    // work_info
+    const workInfo = dcl('ul');
+    workInfo.classList.add('work_info');
+
+    // work_info_item
+    project.frame.forEach((f) => {
+      const workInfoItem = dcl('li');
+      workInfoItem.classList.add('w_info_item');
+      workInfoItem.innerText = f;
+      workInfo.appendChild(workInfoItem);
+    });
+    workDetails.appendChild(workInfo);
+
+    // work_details_content
+    const workDetailsContent = dcl('p');
+    workDetailsContent.classList.add('work_details_content');
+    workDetailsContent.innerText = project.primaryText;
+    workDetails.appendChild(workDetailsContent);
+
+    // work_cat
+    const workCat = dcl('ul');
+    workCat.classList.add('work_cats');
+    project.tags.forEach((tag) => {
+      const catLi = dcl('li');
+      catLi.innerText = tag;
+      workCat.appendChild(catLi);
+    });
+    workDetails.appendChild(workCat);
+    // action button
+    const atnBrn = dcl('button');
+    atnBrn.classList.add('default_btn');
+    atnBrn.innerText = 'See More';
+    atnBrn.setAttribute('id', project.id);
+    atnBrn.addEventListener('click', () => {
+      fetchOnePoject(project.id);
+    });
+    workDetails.appendChild(atnBrn);
+
+    card.appendChild(workDetails);
+    portfolio.appendChild(card);
+  });
+}
+
+window.onload = () => {
+  fetchAllProject();
+};
